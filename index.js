84f30ed9bb1b79ec362c1bd5d604da5861dffe05
Minchul An <min@minchulan.io>
2022-09-13 21:02:03 -0400

    commit tree 8a85fd4832d8068c1906a0fd1cf910f6775004d7

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning to the middle
+  // racecar 7 / 2 == 3.5
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the corresponding letter from the end
+    const j = word.length - 1 - i;
+    //when i is 0, j is 6
+    //when i is 1, j is 5
+    //when i is 2, j is 4
+    //when i is 3, j is 3
+    const startCharacter = word[i];
+    const endCharacter = word[j];
+    // if any letters don't match, return false
+    if (startCharacter !== endCharacter) return false;
+  }
+
+  // return true
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+     ij      
+
+*/
+
+/* 
+ Add your pseudocode here
+
+
 */
 
 /*
